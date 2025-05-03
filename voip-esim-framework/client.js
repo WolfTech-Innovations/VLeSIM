b7dae7cb3a12bc137df51d20069631ffc394deb0
WolfTech Innovations <SpoinkOSGithub@gmail.com>
2025-05-03 17:47:12 +0000

    Commit

--- voip-esim-framework/client.js
+++ voip-esim-framework/client.js
@@ -1,58 +1,12 @@
 const dgram = require('dgram');
-const blessed = require('blessed');
-
 const client = dgram.createSocket('udp4');
-const SERVER_HOST = 'vlesim.onrender.com'; // Remove protocol
+
+// Replace with your server details
+const SERVER_HOST = 'https://vlesim.onrender.com';
 const SERVER_PORT = 5060;
 
-// UI setup
-const screen = blessed.screen({
-  smartCSR: true,
-  title: 'eSIM Provisioner'
-});
-
-const logBox = blessed.log({
-  top: '0',
-  left: 'center',
-  width: '100%',
-  height: '90%',
-  border: 'line',
-  label: ' Logs ',
-  tags: true,
-  scrollable: true,
-  scrollbar: {
-    ch: ' ',
-    track: {
-      bg: 'grey'
-    },
-    style: {
-      inverse: true
-    }
-  }
-});
-
-const statusBar = blessed.box({
-  bottom: 0,
-  height: '10%',
-  width: '100%',
-  content: 'Press {bold}q{/bold} to exit',
-  tags: true,
-  style: {
-    fg: 'white',
-    bg: 'blue'
-  }
-});
-
-screen.append(logBox);
-screen.append(statusBar);
-screen.render();
-
-function log(message) {
-  logBox.log(message);
-  screen.render();
-}
-
 function provisionESIM() {
+  // Create a simple SIP INVITE message for provisioning
   const inviteMessage = 
     'INVITE sip:provision@vlesim.onrender.com SIP/2.0\r\n' +
     'Via: SIP/2.0/UDP client.local;branch=z9hG4bK-test\r\n' +
@@ -68,20 +22,22 @@ function provisionESIM() {
 
   client.send(Buffer.from(inviteMessage), SERVER_PORT, SERVER_HOST, (err) => {
     if (err) {
-      log(`{red-fg}Error sending request: ${err}{/red-fg}`);
+      console.error('Error sending request:', err);
       client.close();
     } else {
-      log('{green-fg}Provisioning request sent{/green-fg}');
+      console.log('Provisioning request sent');
     }
   });
 }
 
+// Handle incoming responses
 client.on('message', (msg, rinfo) => {
-  log(`{cyan-fg}Received response from ${rinfo.address}:${rinfo.port}{/cyan-fg}`);
+  console.log(`Received response from ${rinfo.address}:${rinfo.port}`);
   
   const response = msg.toString();
-  log(`\nResponse:\n${response}`);
+  console.log('\nResponse:\n', response);
   
+  // Extract the relevant information
   if (response.includes('ESIM-PROVISIONED')) {
     const lines = response.split('\n');
     const profileData = {};
@@ -93,23 +49,18 @@ client.on('message', (msg, rinfo) => {
       }
     });
     
-    log(`{yellow-fg}\nProvisioned eSIM Profile:{/yellow-fg}`);
-    log(JSON.stringify(profileData, null, 2));
+    console.log('\nProvisioned eSIM Profile:');
+    console.log(JSON.stringify(profileData, null, 2));
   }
-
+  
   client.close();
 });
 
 client.on('listening', () => {
   const address = client.address();
-  log(`Client listening on ${address.address}:${address.port}`);
+  console.log(`Client listening on ${address.address}:${address.port}`);
   provisionESIM();
 });
 
+// Bind to any available port
 client.bind();
-
-// Exit on 'q'
-screen.key(['q', 'C-c'], function () {
-  client.close();
-  process.exit(0);
-});
